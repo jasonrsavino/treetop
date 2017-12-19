5702054d1658347f2ada5bed95b42543638fb9bb
Jason Savino <jasonrsavino@gmail.com>
2017-12-19 08:54:57 -0800

    patterns

--- grunt/injector.js
+++ grunt/injector.js
@@ -1,23 +1,28 @@
 module.exports = function(grunt) {
 
   grunt.config.set('injector', {
-    options: {
-      prefix: '../../../..',
-      postfix: '?{{ cacheBuster }}'
-    },
     patternlab: {
+      options: {
+        prefix: '../../../..',
+        postfix: '?{{ cacheBuster }}'
+      },
       files: {
         './components/_meta/_00-head.twig': [
-          'css/**/*.css'
+            '../../../core/assets/vendor/normalize-css/normalize.css',
+            '../../../core/themes/stable/css/system/components/*.css',
+            '../../../core/themes/classy/css/components/*.css',
+            'components/css/all.css',
+            'components/css/layouts.css',
+            'css/**/*.css'
         ],
         './components/_meta/_01-foot.twig': [
-          'components/drupal/domready/ready.min.js',
-          'components/drupal/jquery/dist/jquery.min.js',
-          'components/drupal/jquery-once/jquery.once.min.js',
-          'components/drupal/drupal/index.js',
-          'components/drupal/drupalinit/index.js',
-          'components/drupal/drupalSettingsLoader/index.js',
-          'js/**/*.js'
+            'components/drupal/domready/ready.min.js',
+            'components/drupal/jquery/dist/jquery.min.js',
+            'components/drupal/jquery-once/jquery.once.min.js',
+            'components/drupal/drupal/index.js',
+            'components/drupal/drupalinit/index.js',
+            'components/drupal/drupalSettingsLoader/index.js',
+            'js/**/*.js'
         ]
       }
     }
